2d47a791e2fd7c8f62c7b22b5fe14c8cde2a1cd7
zanepetras <zanepetras@cvpcorp.com>
2023-04-28 01:29:32 -0400

    add preliminary regression modelling

--- Health_Data.py
+++ Health_Data.py
@@ -1,15 +1,10 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Apr 27 14:36:57 2023
-
-@author: zanepetras
-"""
-
 import streamlit as st
 import pandas as pd
 from urllib.request import urlopen
 import json
 import plotly.express as px
+from sklearn import linear_model
+import statsmodels.api as sm
 
 with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json') as response:
     counties = json.load(response)
@@ -30,3 +25,15 @@ fig = px.choropleth(deaths, geojson=counties, locations='FIPS', color='DeathsPer
                           )
 fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
 st.write(fig)
+
+DeathsPerCaseEthnicities = df[["DeathsPerCase", "WAC", "BAC", "H", "IAC", "AAC", "NAC"]].dropna()
+X = DeathsPerCaseEthnicities[["WAC", "BAC", "H", "IAC", "AAC", "NAC"]]
+y = DeathsPerCaseEthnicities["DeathsPerCase"]
+
+regr = linear_model.LinearRegression()
+regr.fit(X, y)
+
+X2 = sm.add_constant(X)
+est = sm.OLS(y, X2)
+est2 = est.fit()
+st.write(est2.summary())
